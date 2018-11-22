850bb96538b722f9ae4f8107d5869b7977f820c9
yogi_siswanto <yogisiswanto.c2@gmail.com>
2018-11-22 22:26:23 +0700

    Jam 22.26 Tanggal 22 November 2018

--- app/src/main/java/com/yogisiswanto/submission2/View/Activity/LastMatchFragment.kt
+++ app/src/main/java/com/yogisiswanto/submission2/View/Activity/LastMatchFragment.kt
@@ -1,16 +1,19 @@
-package com.yogisiswanto.submission2.View.Activity
+/*
+ *  Name Class      : LastMatchFragment
+ *  Author          : Yogi Siswanto
+ *  Email           : yogisiswanto.c2@gmail.com
+ *  Language        : Kotlin
+ *  Description     : The Fragment Activity for Last Match TabLayout
+ */
 
+package com.yogisiswanto.submission2.View.Activity
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentActivity
-import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
 import com.google.gson.Gson
 import com.yogisiswanto.submission2.API.ApiRespository
 import com.yogisiswanto.submission2.Adapter.MainAdapter
@@ -24,17 +27,16 @@ import com.yogisiswanto.submission2.View.MainView
 import org.jetbrains.anko.support.v4.onRefresh
 import kotlinx.android.synthetic.main.fragment_last_match.*
 
-/**
- * A simple [Fragment] subclass.
- *
- */
+//Class LastMatchFragment
 class LastMatchFragment : Fragment(), MainView {
 
+    //variable instantiation
     private var teams: MutableList<Event> = mutableListOf()
     private lateinit var presenter: MainPresenter
     private lateinit var adapter: MainAdapter
     private lateinit var league: String
 
+    //creating Fragment Layout
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -44,49 +46,57 @@ class LastMatchFragment : Fragment(), MainView {
     }
 
 
-
+    //when activity created, accessing data from API
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        val llm = LinearLayoutManager(this.context)
-        llm.orientation = LinearLayoutManager.VERTICAL
+        //RecyclerView.LayoutManager instantiation
+        listTeam.layoutManager = LinearLayoutManager(context)
 
-        listTeam.layoutManager = llm
+        //RecyclerView adapter instantiation
         adapter = MainAdapter(teams)
         listTeam.adapter = adapter
+
+        //instantiation Id League
         league = "4335"
 
+        //local variable instantiation
         val request = ApiRespository()
         val gson = Gson()
+
+        //Presenter instantiation
         presenter = MainPresenter(this, request, gson)
-        presenter.getTeamList(league)
+
+        //accessing The API from presenter
+        presenter.get15LastEventsList(league)
 
 
+        //when SwipeRefresh is pull, the data will be call
         swipeRefresh.onRefresh {
 
-            presenter.getTeamList(league)
+            presenter.get15LastEventsList(league)
         }
 
     }
 
-
+    //show loading
     override fun showLoading() {
 
         progressBar.visible()
     }
 
+    //hidden loading
     override fun hideLoading() {
 
         progressBar.invisble()
     }
 
-    override fun showTeamList(data: List<Event>) {
+    //show data from API
+    override fun showEventList(data: List<Event>) {
 
         swipeRefresh.isRefreshing = false
         teams.clear()
         teams.addAll(data)
         adapter.notifyDataSetChanged()
     }
-
-
 }
